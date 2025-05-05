e0a7a5dccfe979f0840550919c96a87319bde320
AfifiNL <hossam.afifiiii@gmail.com>
2025-05-05 17:42:12 +0200

    Add utility functions including class name handling

--- isystem-main-website/src/lib/utils.ts
+++ isystem-main-website/src/lib/utils.ts
@@ -1,34 +1,10 @@
-import { clsx, type ClassValue } from 'clsx';
+import { ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 /**
- * Combines multiple class names using clsx and tailwind-merge for efficient class management
+ * Combines class names and merges Tailwind classes efficiently
+ * Used throughout the application for component styling
  */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-/**
- * Formats a price value with the given currency
- */
-export function formatPrice(price: number, currency: string = 'EUR', locale: string = 'nl-NL') {
-  return new Intl.NumberFormat(locale, {
-    style: 'currency',
-    currency,
-  }).format(price);
-}
-
-/**
- * Creates a debounced function that will only execute after a certain delay
- */
-export function debounce<T extends (...args: any[]) => any>(
-  fn: T,
-  delay: number
-): (...args: Parameters<T>) => void {
-  let timeoutId: NodeJS.Timeout;
-  
-  return function (...args: Parameters<T>) {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => fn(...args), delay);
-  };
 }
